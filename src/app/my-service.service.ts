7906d85a0c5cee437a888cfefc0babd04f5dc8c4
mrsfreddy <rmogusu123@gmal.com>
2019-09-30 00:55:17 +0300

    Generate user class where i will define my user properties

--- src/app/my-service.service.ts
+++ src/app/my-service.service.ts
@@ -1,48 +1,48 @@
-// import {Injectable} from '@angular/core';
-// import {HttpClient} from '@angular/common/http';
-// import {environment} from '../environments/environment';
-// import {GitUser} from './git-user';
-// import {SearchUserComponent} from './search-user/search-user.component';
-// import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {GitUser} from './git-user';
+import {SearchUserComponent} from './search-user/search-user.component';
+import {Observable} from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class MyServiceService {
-//   user: GitUser[] = [];
-//   _URL = 'https://api.github.com/users/';
-//   token = "?access_token=e8d69983bba3ae132ca4cf6f36bba724aefac099";
+@Injectable({
+  providedIn: 'root'
+})
+export class MyServiceService {
+  user: GitUser[] = [];
+  _URL = 'https://api.github.com/users/';
+  token = "?access_token=e8d69983bba3ae132ca4cf6f36bba724aefac099";
 
-//   constructor(private http: HttpClient) {
-//   }
+  constructor(private http: HttpClient) {
+  }
 
-//   searchMyUser(searchTerm: string) {
+  searchMyUser(searchTerm: string) {
 
-//     interface data {
-//       login: string;
-//       avatar_url: string;
-//       following: string;
-//       followers: string;
-//       public_repos: string;
-//       location:string;
-//       company:string;
-//       bio:string;
+    interface data {
+      login: string;
+      avatar_url: string;
+      following: string;
+      followers: string;
+      public_repos: string;
+      location:string;
+      company:string;
+      bio:string;
      
-//     }
+    }
 
-//     return new Promise((resolve, reject) => {
-//       this.user = [];
+    return new Promise((resolve, reject) => {
+      this.user = [];
 
-//       this.http.get<data>(this._URL + searchTerm + this.token).toPromise().then(
-//         (results) => {
+      this.http.get<data>(this._URL + searchTerm + this.token).toPromise().then(
+        (results) => {
           
-//           this.user.push(results);
-//           resolve();
-//         },
-//         (error) => {
-//           reject();
-//         }
-//       );
-//     });
-//   }
-// }
+          this.user.push(results);
+          resolve();
+        },
+        (error) => {
+          reject();
+        }
+      );
+    });
+  }
+}
