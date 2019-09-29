bfc75c6c644f7df9ca8396ec445b288a93262abd
mrsfreddy <rmogusu123@gmal.com>
2019-09-30 01:15:20 +0300

    Add custome directive to show the time the repo was created and days that have passed since then

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import {UserDataComponent} from './user-data/user-data.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {UserRepoListComponent} from './user-repo-list/user-repo-list.component';
 
-// const routes: Routes = [
-//   {path: 'user-list', component: UserDataComponent},
-//   {path: 'repo-list', component: UserRepoListComponent},
-//   {path: '', redirectTo: '/user-list', pathMatch: 'full'},
-//   {path: '**', component : NotFoundComponent}
-// ];
+const routes: Routes = [
+  {path: 'user-list', component: UserDataComponent},
+  {path: 'repo-list', component: UserRepoListComponent},
+  {path: '', redirectTo: '/user-list', pathMatch: 'full'},
+  {path: '**', component : NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
