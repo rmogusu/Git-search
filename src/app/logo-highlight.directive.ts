7ba90a10c77ed86e4c63a9402c9ca8663bdd8677
mrsfreddy <rmogusu123@gmal.com>
2019-09-30 01:19:33 +0300

    Add images to assets  which i will use to style my container to make my app user friendy

--- src/app/logo-highlight.directive.ts
+++ src/app/logo-highlight.directive.ts
@@ -3,15 +3,15 @@ import { Directive ,ElementRef,HostListener} from '@angular/core';
 @Directive({
   selector: '[appLogoHighlight]'
 })
-// export class LogoHighlightDirective {
-//   constructor(private elem:ElementRef) {}
-//   @HostListener("click") onClicks(){
-//   this.textDeco("aqua")
-//   }
+export class LogoHighlightDirective {
+  constructor(private elem:ElementRef) {}
+  @HostListener("click") onClicks(){
+  this.textDeco("aqua")
+  }
 
-//   @HostListener("dblclick") onDoubleClicks(){
-//     this.textDeco("transparent")
-//   }
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("transparent")
+  }
 
 
   private textDeco(action:string){
